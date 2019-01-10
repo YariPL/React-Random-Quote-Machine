fcaeb8733e0e75767cebea3e79ea2be1dcdd6e5e
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-10 02:29:41 +0100

    update css for quotebox

--- src/index.js
+++ src/index.js
@@ -3,27 +3,35 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Quote extends React.Component {
+	
 	render() {
 		return(
 			<div id='quotePart'>
-
+				<div id="text">
+				</div>
+				<div id="author">
+				</div>
 			</div>
 		)
 	}
 }
 
 class QuoteBox extends React.Component {
+	constructor() {
+		super();
+	}
 	render() {
 		return(
 			<div id="quote-box">
 				<Quote />
 				<div id='buttons'>
 					<div id="tweet-quote">
-						Tweet
+						<div class='button'>Tweet</div>
 					</div>
 					<div id="new-quote">
-						New Quote
+						<div class='button'>New Quote</div>
 					</div>
+				</div>
 			</div>
 		)
 	}
