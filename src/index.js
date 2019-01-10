555637227ca0a1cf4d8acbddd25aaa299effd7d1
Yari PL <yaroslav.khudchenko@gmail.com>
2019-01-10 01:56:47 +0100

    added QuateBox and Quate components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Quote extends React.Component {
+	render() {
+		return(
+			<div>
+
+			</div>
+		)
+	}
+}
+
+class QuoteBox extends React.Component {
+	render() {
+		return(
+			<div id="quote-box">
+				<Quote />
+				<div id='buttons'>
+					<div id="tweet-quote">
+						Tweet
+					</div>
+					<div id="new-quote">
+						New Quote
+					</div>
+			</div>
+		)
+	}
+}
 
 
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<QuoteBox />, document.getElementById('root'));
